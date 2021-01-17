71e2e5f36f80f67fdf0dc58ffd5d237a5bbe9b66
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-01-16 21:37:39 -0500

    first go at schemata

---
+++ employeesql/schemata.sql
@@ -0,0 +1,53 @@
+-- create table titles (
+-- 	title_id varchar primary key,
+-- 	title varchar
+-- );
+
+-- select * from titles;
+
+-- create table employees (
+-- 	emp_no int primary key,
+-- 	emp_title_id varchar,
+-- 	birth_date date,
+-- 	first_name varchar,
+-- 	last_name varchar,
+-- 	sex varchar(1),
+-- 	hire_date date,
+-- 	foreign key (emp_title_id) references titles(title_id)
+-- );
+
+-- select * from employees;
+
+-- create table salaries (
+-- 	emp_no int primary key, 
+-- 	foreign key (emp_no) references employees(emp_no),
+-- 	salary int
+-- );
+
+-- select * from salaries;
+
+-- create table dept_manager (
+-- 	dept_no varchar,
+-- 	emp_no int primary key,
+-- 	foreign key (emp_no) references employees (emp_no)
+-- );
+
+-- select * from dept_manager;
+
+-- create table departments (
+--  	dept_no varchar primary key,
+--  	dept_name varchar
+-- );
+
+-- select * from departments;
+
+-- !having trouble here
+-- create table dept_emp (
+-- 	emp_no serial primary key,
+-- 	dept_no varchar,
+-- 	foreign key (emp_no) references employees (emp_no),
+-- 	foreign key (dept_no) references departments (dept_no)
+-- );
+
+select * from dept_emp;
+-- drop table dept_emp;
