71b0f3088ee9badde0092029ca82102d4fec2a8f
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-01-18 16:46:30 -0500

    3rd query

--- employeesql/queries.sql
+++ employeesql/queries.sql
@@ -5,6 +5,17 @@
 -- from employees;
 
 --List first name, last name, and hire date for employees who were hired in 1986
-select first_name, last_name, hire_date
-from employees
-where extract (year from hire_date) = 1986;
+-- select first_name, last_name, hire_date
+-- from employees
+-- where extract (year from hire_date) = 1986;
+
+--List manager of each department with: department number, department name, the manager's employee number, last name, first name
+select dept_manager.dept_no, (
+select departments.dept_name from departments where departments.dept_no = dept_manager.dept_no
+), (
+select employees.last_name from employees where employees.emp_no = dept_manager.emp_no
+), (
+select employees.first_name from employees where employees.emp_no = dept_manager.emp_no
+),
+dept_manager.emp_no
+from dept_manager;
