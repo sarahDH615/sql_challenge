38debb89c9bd6c968da40bb1d6e99771e9a9207e
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-01-21 12:14:13 -0500

    more annotations for schemata

--- employeesql/schemata.sql
+++ employeesql/schemata.sql
@@ -27,6 +27,10 @@ create table employees (
 --checking whether the columns were added to the employees table properly
 select * from employees;
 
+--create salaries table
+--both cols take integers
+--emp_no col matches emp_no col in employees table (foreign key)
+--but emp_no col also has unique values for this table, so it is also the primary key
 create table salaries (
 	emp_no int primary key, 
 	foreign key (emp_no) references employees(emp_no),
@@ -36,6 +40,8 @@ create table salaries (
 --checking whether the columns were added to the salaries table properly
 select * from salaries;
 
+--create departments table
+--dept_no has unique ids (primary key)
 create table departments (
  	dept_no varchar primary key,
  	dept_name varchar
@@ -44,16 +50,26 @@ create table departments (
 --checking whether the columns were added to the departments table properly
 select * from departments;
 
+--create dept_manager table
+--both cols in this table reference other tables' primary keys (junction table)
+    --emp_no references emp_no in employees table (foreign key)
+    --dept_no references dept_no in department table (foreign key)
+--emp_no has unique values for this table (primary key)
 create table dept_manager (
 	dept_no varchar,
 	emp_no int primary key,
 	foreign key (emp_no) references employees (emp_no),
- foreign key (dept_no) references departments (dept_no)
+    foreign key (dept_no) references departments (dept_no)
 );
 
 --checking whether the columns were added to the dept_manager table properly
 select * from dept_manager;
 
+--create dept_emp table
+--neither col is unique on its own, so a composite key must be made
+--both cols are primary keys in other tables (both are foreign keys)(junction table)
+    -- emp_no references emp_no in employees table
+    -- dept_no references dept_no in departments
 create table dept_emp (
 	emp_no serial,
 	dept_no varchar,
